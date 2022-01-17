43aeb7823e92b714c59584691cad82c58ad54648
raichek23 <raichek23@gmail.com>
2022-01-17 23:50:15 +0000

    new/delete

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ POS::~POS()
 
 void POS::print()
 {
-    printf("x:%lf y:%lf", x, y);
+    printf("x:%lf y:%lf\n", x, y);
 }
 
 Human::Human(char* name, int age)
@@ -46,16 +46,33 @@ Human::Human(char* name, int age)
 }
 int main()
 {
-    POS pos;
+    POS * pPos = new POS;
 
-    pos.x = 100.0;
-    pos.y = 200.0;
+    pPos->x = 100.0;
+    pPos->y = 200.0;
 
-    pos.print();
+    pPos->print();
     
     char name[] = "Taro Yamada";
 
     Human human1(name, 20);
+
+    int * pNum1    = new int;
+    short * pNum2  = new short(50);
+    short * pArray = new short[5];
+
+    *pNum1 = 100;
+    printf("Num1:%d Num2:%d\n", *pNum1, *pNum2);
+
+    for (int i = 0; i < 5; i++)
+    {
+        pArray[i] = i;
+        printf("pArray[%d]:%d\n", i, i);
+    }
+
+    delete pNum1;
+    delete pNum2;
+    delete pPos;
 
     return 0;
 }
